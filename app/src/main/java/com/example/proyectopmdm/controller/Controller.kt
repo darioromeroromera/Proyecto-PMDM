a5ac7b46c9b4dcf85a901b8ae6eb43489c5ad53a
Darío Romero Romera <darioromeroromera@gmail.com>
2023-12-13 12:22:33 +0100

    Añadido dialog de crear contacto

--- app/src/main/java/com/example/proyectopmdm/controller/Controller.kt
+++ app/src/main/java/com/example/proyectopmdm/controller/Controller.kt
@@ -1,36 +1,61 @@
 package com.example.proyectopmdm.controller
 
 import android.content.Context
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.proyectopmdm.MainActivity
 import com.example.proyectopmdm.adapter.ContactoAdapter
 import com.example.proyectopmdm.dao.ContactosDao
+import com.example.proyectopmdm.dialogues.CreateDialogue
 import com.example.proyectopmdm.dialogues.DeleteDialogue
 import com.example.proyectopmdm.models.Contacto
 
 class Controller(val context: Context){
     lateinit var listaContactos: MutableList<Contacto>
     lateinit var adapter : ContactoAdapter
+    lateinit var mainActivity: MainActivity
+    lateinit var layoutManager: LinearLayoutManager
 
     init {
         listaContactos = ContactosDao.dao.getContactsData().toMutableList()
+        mainActivity = context as MainActivity
+        initLayoutManager()
         setAdapter()
+        initEvent()
+    }
+
+    private fun initLayoutManager() {
+        layoutManager = mainActivity.binding.rvContactos.layoutManager as LinearLayoutManager
     }
 
     fun setAdapter() {
-        val myActivity = context as MainActivity
         adapter = ContactoAdapter(listaContactos, {
                 pos -> delContact(pos)
         })
-        myActivity.binding.rvContactos.adapter = adapter
+        mainActivity.binding.rvContactos.adapter = adapter
+    }
+
+    private fun initEvent() {
+        mainActivity.binding.fbAdd.setOnClickListener { view ->
+            addContacto()
+        }
+    }
+
+    private fun addContacto() {
+        val dialog = CreateDialogue(
+
+            {
+            contact -> okOnCreateContact(contact)
+        })
+        dialog.show(mainActivity.supportFragmentManager, "Añadir")
+    }
+
+    private fun okOnCreateContact(contact: Contacto) {
+        listaContactos.add(contact)
+        adapter.notifyItemInserted(listaContactos.lastIndex)
+        layoutManager.scrollToPositionWithOffset(listaContactos.lastIndex, 20)
     }
 
     private fun delContact(pos: Int) {
-        /*listaContactos.removeAt(pos)
-        adapter.notifyItemRemoved(pos)
-        adapter.notifyItemChanged(pos)
-        adapter.notifyDataSetChanged()*/
-
-        val mainActivity = context as MainActivity
 
         val dialog = DeleteDialogue(
             pos,
@@ -38,7 +63,7 @@ class Controller(val context: Context){
             {
             pos -> okOnDeleteContact(pos)
         })
-        dialog.show(mainActivity.supportFragmentManager, "Borrado")
+        dialog.show(mainActivity.supportFragmentManager, "Borrar")
 
     }
 
--- app/src/main/java/com/example/proyectopmdm/dialogues/CreateDialogue.kt
+++ app/src/main/java/com/example/proyectopmdm/dialogues/CreateDialogue.kt
@@ -1,4 +1,56 @@
 package com.example.proyectopmdm.dialogues
 
-class CreateDialogue {
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.example.proyectopmdm.R
+import com.example.proyectopmdm.databinding.DialogLayoutBinding
+import com.example.proyectopmdm.models.Contacto
+import java.lang.IllegalStateException
+
+class CreateDialogue(
+    var okOnCreateContact: (Contacto) -> Unit
+) : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let { activity ->
+            val builder = AlertDialog.Builder(activity)
+            val inflater = activity.layoutInflater
+            val viewContacto = inflater.inflate(R.layout.dialog_layout, null)
+            builder.setView(viewContacto)
+            builder.setMessage("Añadir Contacto")
+            builder.setPositiveButton("Añadir",
+                DialogInterface.OnClickListener { dialog, id ->
+                    val contact = createContact(viewContacto)
+
+                    if (isContactFilled(contact))
+                        okOnCreateContact(contact)
+                    else
+                        Toast.makeText(activity, "Todos los campos excepto imagen son obligatorios", Toast.LENGTH_LONG).show()
+                })
+            builder.setNegativeButton("Cancelar",
+                DialogInterface.OnClickListener { dialog, id ->
+                    Toast.makeText(activity, "Operación cancelada", Toast.LENGTH_LONG).show()
+                    dialog?.dismiss()
+                })
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    private fun createContact(view: View) : Contacto {
+        val binding = DialogLayoutBinding.bind(view)
+        return Contacto(
+            binding.etNombre.text.toString(),
+            binding.etNombreCompleto.text.toString(),
+            binding.etTelefono.text.toString(),
+            binding.etImagen.text.toString()
+        )
+    }
+
+    private fun isContactFilled(contact: Contacto) : Boolean {
+        return (! (contact.nombre.isNullOrBlank() || contact.nombreCompleto.isNullOrBlank() || contact.telefono.isNullOrBlank()))
+    }
 }
